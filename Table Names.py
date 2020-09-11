6b5ec0f4899fcebeabc619fda84ee7bfbf9ba348
Madhav Kannan <mkannan@tableau.com>
2020-09-11 10:46:09 +0800

    Removing lists and writing to file directly

--- Table Names.py
+++ Table Names.py
@@ -2,17 +2,16 @@ import tableauserverclient as TSC
 import csv
 
 #Server Details#
-SERVER_NAME = ""
+SERVER_NAME = "http://"
 SITE_NAME = ""
 USERNAME = ""
 PASSWORD = ""
-#Creating a TSC object#
-tableau_auth = TSC.TableauAuth(USERNAME, PASSWORD, SITE_NAME)
-server = TSC.Server(SERVER_NAME)
-server.version = '3.5'
-#Initializing list elements for writing into CSV later#
-datasource_list, workbook_list, tableName_list, connectionType_list = ([] for i in range(4))
-sqlQuery_list, extract_list, publishedDatasource_list, dbName_list = ([] for i in range(4))
+
+#Providing column headers for CSV file#
+csvfile  = open("tables.csv", "w+")
+table_writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+table_writer.writerow(['Workbook Name', 'Datasource Name', 'Published Datasource?', 'Extract?',
+                       'Table Name', 'Database Name', 'Connection Type', 'Custom SQL?'])	
 
 #Metadata API query#
 query = '''
@@ -52,6 +51,10 @@ query = '''
     }
 }
 '''
+#Creating a TSC object#
+tableau_auth = TSC.TableauAuth(USERNAME, PASSWORD, SITE_NAME)
+server = TSC.Server(SERVER_NAME)
+server.version = '3.5'
 
 #Signing into REST API and executing the metadata query#
 with server.auth.sign_in(tableau_auth):
@@ -73,28 +76,19 @@ with server.auth.sign_in(tableau_auth):
         for datasource in datasources:
             if datasource['upstreamTables']:
                 for counter in range (0, len(datasource['upstreamTables'])):
+                    is_published = 'False'
+                    sql_query = ''
                     if datasource in published_datasources:
-                        publishedDatasource_list.append ('True')
-                    else:
-                        publishedDatasource_list.append ('False')
-                    workbook_list.append (workbook['name'])
-                    datasource_list.append (datasource['name'])
-                    extract_list.append (datasource['hasExtracts'])
-                    tableName_list.append (datasource['upstreamTables'][counter]['name'])
-                    dbName_list.append (datasource['upstreamTables'][counter]['database']['name'])
-                    connectionType_list.append (datasource['upstreamTables'][counter]['connectionType'])
+                        is_published = 'True'
+                    workbook_name = workbook['name']
+                    datasource_name = datasource['name']
+                    has_extract = datasource['hasExtracts']
+                    table_name = datasource['upstreamTables'][counter]['name']
+                    db_name = datasource['upstreamTables'][counter]['database']['name']
+                    connection_type = datasource['upstreamTables'][counter]['connectionType']
                     if datasource['upstreamTables'][counter]['referencedByQueries']:
-                        sqlQuery_list.append (datasource['upstreamTables'][counter]['referencedByQueries'][0]['query'])
-                    else:
-                        sqlQuery_list.append ('')
-
-#Writing all the information into a CSV file#
-with open("tables.csv", "w+") as csvfile:
-    table_writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-    table_writer.writerow(['Workbook Name', 'Datasource Name', 'Published Datasource?', 'Extract?',
-                           'Table Name', 'Database Name', 'Connection Type', 'Custom SQL?'])	
-    for counter in range (0, len(datasource_list)):
-        table_writer.writerow([workbook_list[counter], datasource_list[counter], 
-                               publishedDatasource_list[counter], extract_list[counter], 
-                               tableName_list[counter], dbName_list[counter],
-                               connectionType_list[counter], sqlQuery_list[counter]])
+                        sql_query = datasource['upstreamTables'][counter]['referencedByQueries'][0]['query']
+                    #Writing to CSV#
+                    table_writer.writerow([workbook_name, datasource_name, 
+                                is_published, has_extract, table_name, 
+                                db_name, connection_type, sql_query])
